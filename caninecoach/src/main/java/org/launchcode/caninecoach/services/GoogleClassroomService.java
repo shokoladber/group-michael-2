14c6ff6827027c2cb4fbd3215eb0f7cedbbf7cde
lucydames <lucydames1254@gmail.com>
2024-02-03 15:12:52 -0600

    api integration

---
+++ caninecoach/src/main/java/org/launchcode/caninecoach/services/GoogleClassroomService.java
@@ -0,0 +1,56 @@
+package org.launchcode.caninecoach.services;
+
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.classroom.Classroom;
+import com.google.api.services.classroom.model.ListCoursesResponse;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+
+@Service
+public class GoogleClassroomService {
+
+
+    private final ResourceLoader resourceLoader;
+    private Classroom classroom;
+
+
+    public GoogleClassroomService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+
+    @PostConstruct
+    public void init() throws IOException, GeneralSecurityException {
+        InputStream serviceAccountStream = resourceLoader.getResource("classpath:google-service-account.json").getInputStream();
+
+
+        // Authenticate using the service account for Google Classroom API
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream)
+                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/classroom.courses.readonly"));
+
+
+        // Build the Classroom service
+        classroom = new Classroom.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), new HttpCredentialsAdapter(credentials))
+                .setApplicationName("Canine Coach")
+                .build();
+    }
+
+
+    // Example method: List courses
+    public ListCoursesResponse listCourses() throws IOException {
+        Classroom.Courses.List request = classroom.courses().list();
+        return request.execute();
+    }
+}
