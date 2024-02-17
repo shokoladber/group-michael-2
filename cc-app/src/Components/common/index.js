a0c9a4651b8a0da961f1235f5078072e30cbd264
lucydames <lucydames1254@gmail.com>
2024-02-17 15:20:30 -0600

    merging landing updates

---
+++ cc-app/src/Components/common/index.js
@@ -0,0 +1,13 @@
+export { default as Header} from './header/Header';
+export { default as Navbar} from './navbar/Navbar';
+export { default as LoginSignup} from './LoginSignup/LoginSignup';
+export { default as BlogPost} from './blogpost/BlogPost';
+export { default as PetProfilePage} from './petProfilePage/PetProfilePage';
+export { default as FileUpload} from './fileUpload/FileUpload';
+export { default as ContactUs} from './contact2/ContactUs';
+export { default as Button} from './button/Button';
+export { default as Dropdown} from './dropdown/Dropdown';
+export { default as BuyClasses} from './buy/BuyClasses';
+export { default as Footer} from './footer/Footer';
+
+
