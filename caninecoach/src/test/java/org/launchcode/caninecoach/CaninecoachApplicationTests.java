b430a895a7e7916662a4725634a933a2f4d5bbc7
lucydames <135853701+lucydames@users.noreply.github.com>
2024-02-21 07:27:15 -0600

    update merge

--- caninecoach/src/test/java/org/launchcode/caninecoach/CaninecoachApplicationTests.java
+++ caninecoach/src/test/java/org/launchcode/caninecoach/CaninecoachApplicationTests.java
@@ -2,7 +2,6 @@ package org.launchcode.caninecoach;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class CanineCoachApplicationTests {
